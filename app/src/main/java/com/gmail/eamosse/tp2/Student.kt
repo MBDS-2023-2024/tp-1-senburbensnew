d12b35644a933fc03daa1a24d65a4804bb050dca
pierre rubens milorme <prubensmilorme@gmail.com>
2024-07-22 20:54:01 -0400

    creation classe etudiant

--- app/src/main/java/com/gmail/eamosse/tp2/Student.kt
+++ app/src/main/java/com/gmail/eamosse/tp2/Student.kt
@@ -1,6 +1,19 @@
 package com.gmail.eamosse.tp2
 
 //TODO : Complete the class
-data class Student {
-
+data class Student(
+    val name: String,
+    val code: String,
+    val sexe: Char,
+    val adresse: String,
+    val age: Int
+) {
+    init {
+        require(sexe == 'M' || sexe == 'F') {
+            "Sexe doit être 'M' ou 'F'"
+        }
+        require(age >= 0) {
+            "L'âge doit être un nombre positif"
+        }
+    }
 }
